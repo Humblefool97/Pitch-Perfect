5e0802df01099284991eefdbc2a0e3015f93e4da
JobsJr <rajeevkr878@gmail.com>
2017-04-02 01:35:49 +0530

    Wiringup buttons

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -11,27 +11,28 @@ import UIKit
 class PlaySoundsViewController: UIViewController {
     var recordedAudioURL: URL!
     
+    @IBOutlet weak var slowButton: UIButton!
+    @IBOutlet weak var fastButton: UIButton!
+    @IBOutlet weak var highPitchButton: UIButton!
+    @IBOutlet weak var lowPitchButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func startPlayback(_ sender: UIButton) {
+        print("Playback started..")
+        
     }
-    */
+    
+    @IBAction func stopPlayback(_ sender: UIButton) {
+        print("Playback stopped..")
+
+    }
+
 
 }
